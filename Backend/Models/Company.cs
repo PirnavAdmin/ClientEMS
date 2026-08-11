53d686ec4b397a3061b17802813c5dcd7b037892
vijithaputluru <vijithaputluru2003@gmail.com>
2026-08-11 21:46:30 +0530

    Update Company.cs

--- Backend/Models/Company.cs
+++ Backend/Models/Company.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManagementSystem.Models
 {
-    [Table("Company")]
+    [Table("company")]
     public class Company
     {
         [Key]
